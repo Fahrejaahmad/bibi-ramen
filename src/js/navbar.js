79f17a485bdf8e1f63a8edd9a4372701c787f035
Fahreja <112296133+Fahrejaahmad@users.noreply.github.com>
2023-07-14 08:31:48 +0700

    menambahkan menu dan memisahkan beberapa file

---
+++ src/js/navbar.js
@@ -0,0 +1,18 @@
+// Javasript untuk Navbar
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+const bgNav = document.querySelector('nav')
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+    bgNav.classList.toggle('bgnav')
+});
+window.addEventListener('scroll', function() {
+  let header = document.getElementById('header');
+
+  if (window.pageYOffset > 0) {
+      header.style.backgroundColor = '#567189';
+      header.style.transition = 'all 1ms'
+  } else {
+      header.style.backgroundColor = ''; 
+  }
+});
